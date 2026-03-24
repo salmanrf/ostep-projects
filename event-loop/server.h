cddd48a04f0294b9c71a437cfadb565f441919de
salmanrf <salmanrf2@gmail.com>
2026-03-24 15:41:44 +0700

    feat: event-loop with poll

--- event-loop/server.h
+++ event-loop/server.h
@@ -1,7 +1,13 @@
+#include <poll.h>
 #include "common.h"
+
+#define INIT_POLLFDS_SIZE 10
+#define POLL_TIMEOUT_MS 5000
 
 void handle_conn(int client_fd, struct sockaddr *client_addr, int addrsize);
 
 void handle_connected(int client_fd);
 
 int handle_received_from_client(int client_fd);
+
+struct pollfd *build_pollfds(int numfds, int startfd, short events);
